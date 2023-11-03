0620ec67e2e6ff5f19b293535baa78baef0c5161
kevinLjohn <108888414+kevinLjohn@users.noreply.github.com>
2023-11-03 14:28:47 +0000

    project

---
+++ soucrecode/README.md
@@ -0,0 +1,5 @@
+# Kevin_cloneRepo
+
+This is my project. the function will allow you to create a responce to your boss on why you can't come into work
+
+
---
+++ soucrecode/kevin_gitClonefunction.py
@@ -0,0 +1,7 @@
+
+def getoutofwork():
+    reason = input("Please type the reason why you don't wanna work today ")
+    time = (input("input the time ypour shift starts "))
+    print("sorry boss i can't go to work today because " + reason + " so i can't come today at " + time )
+
+getoutofwork()
